3f94d242714e4283878c9dbca0cfd939c1861603
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2022-05-04 15:35:03 +0200

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  config.assets.debug = true
+  # config.assets.debug = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
